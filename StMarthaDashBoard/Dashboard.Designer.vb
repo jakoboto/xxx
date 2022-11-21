694fc3d6d8a5f636db307f686ec76db0f7ef3246
Nico <noverojohnnico@gmail.com>
2022-11-21 11:20:32 +0800

    :art: Add login and dashboard UI

---
+++ StMarthaDashBoard/Dashboard.Designer.vb
@@ -0,0 +1,437 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Dashboard
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Dashboard))
+        Me.p_left = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.dash_holder = New System.Windows.Forms.Panel()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel7 = New System.Windows.Forms.Panel()
+        Me.Panel12 = New System.Windows.Forms.Panel()
+        Me.Panel13 = New System.Windows.Forms.Panel()
+        Me.Panel11 = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel10 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.side_b1 = New System.Windows.Forms.Panel()
+        Me.home_btn = New System.Windows.Forms.Button()
+        Me.Panel8 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel9 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.stats_panel = New System.Windows.Forms.Panel()
+        Me.Panel16 = New System.Windows.Forms.Panel()
+        Me.Panel15 = New System.Windows.Forms.Panel()
+        Me.Panel17 = New System.Windows.Forms.Panel()
+        Me.panel_stat3 = New System.Windows.Forms.Panel()
+        Me.panel_stat2 = New System.Windows.Forms.Panel()
+        Me.panel_stat1 = New System.Windows.Forms.Panel()
+        Me.dash_holder.SuspendLayout()
+        Me.Panel5.SuspendLayout()
+        Me.Panel7.SuspendLayout()
+        Me.Panel12.SuspendLayout()
+        Me.Panel11.SuspendLayout()
+        Me.Panel10.SuspendLayout()
+        Me.side_b1.SuspendLayout()
+        Me.Panel8.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.Panel9.SuspendLayout()
+        Me.stats_panel.SuspendLayout()
+        Me.Panel16.SuspendLayout()
+        Me.Panel15.SuspendLayout()
+        Me.Panel17.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'p_left
+        '
+        Me.p_left.BackColor = System.Drawing.Color.White
+        Me.p_left.Dock = System.Windows.Forms.DockStyle.Left
+        Me.p_left.Location = New System.Drawing.Point(0, 0)
+        Me.p_left.Name = "p_left"
+        Me.p_left.Size = New System.Drawing.Size(22, 583)
+        Me.p_left.TabIndex = 1
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.White
+        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel3.Location = New System.Drawing.Point(22, 561)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(1216, 22)
+        Me.Panel3.TabIndex = 4
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.White
+        Me.Panel4.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel4.Location = New System.Drawing.Point(1216, 23)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(22, 538)
+        Me.Panel4.TabIndex = 5
+        '
+        'dash_holder
+        '
+        Me.dash_holder.BackColor = System.Drawing.SystemColors.Control
+        Me.dash_holder.Controls.Add(Me.stats_panel)
+        Me.dash_holder.Controls.Add(Me.Panel5)
+        Me.dash_holder.Controls.Add(Me.Panel2)
+        Me.dash_holder.Controls.Add(Me.Panel7)
+        Me.dash_holder.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dash_holder.Location = New System.Drawing.Point(22, 23)
+        Me.dash_holder.Name = "dash_holder"
+        Me.dash_holder.Size = New System.Drawing.Size(1194, 538)
+        Me.dash_holder.TabIndex = 6
+        '
+        'Panel5
+        '
+        Me.Panel5.BackColor = System.Drawing.Color.MidnightBlue
+        Me.Panel5.Controls.Add(Me.Label2)
+        Me.Panel5.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel5.Location = New System.Drawing.Point(290, 0)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(904, 67)
+        Me.Panel5.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(19, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(174, 36)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Dashboard"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel2.Location = New System.Drawing.Point(280, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(10, 538)
+        Me.Panel2.TabIndex = 1
+        '
+        'Panel7
+        '
+        Me.Panel7.BackColor = System.Drawing.Color.MidnightBlue
+        Me.Panel7.Controls.Add(Me.Panel12)
+        Me.Panel7.Controls.Add(Me.Panel11)
+        Me.Panel7.Controls.Add(Me.Panel10)
+        Me.Panel7.Controls.Add(Me.side_b1)
+        Me.Panel7.Controls.Add(Me.Panel8)
+        Me.Panel7.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel7.Location = New System.Drawing.Point(0, 0)
+        Me.Panel7.Name = "Panel7"
+        Me.Panel7.Size = New System.Drawing.Size(280, 538)
+        Me.Panel7.TabIndex = 0
+        '
+        'Panel12
+        '
+        Me.Panel12.Controls.Add(Me.Panel13)
+        Me.Panel12.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel12.Location = New System.Drawing.Point(0, 496)
+        Me.Panel12.Name = "Panel12"
+        Me.Panel12.Size = New System.Drawing.Size(280, 42)
+        Me.Panel12.TabIndex = 5
+        '
+        'Panel13
+        '
+        Me.Panel13.BackgroundImage = CType(resources.GetObject("Panel13.BackgroundImage"), System.Drawing.Image)
+        Me.Panel13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Panel13.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel13.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel13.Location = New System.Drawing.Point(0, 0)
+        Me.Panel13.Name = "Panel13"
+        Me.Panel13.Size = New System.Drawing.Size(55, 42)
+        Me.Panel13.TabIndex = 0
+        '
+        'Panel11
+        '
+        Me.Panel11.Controls.Add(Me.Button2)
+        Me.Panel11.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel11.Location = New System.Drawing.Point(0, 184)
+        Me.Panel11.Name = "Panel11"
+        Me.Panel11.Size = New System.Drawing.Size(280, 64)
+        Me.Panel11.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.BackColor = System.Drawing.SystemColors.Control
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.FlatAppearance.BorderSize = 0
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.Button2.Location = New System.Drawing.Point(27, 8)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(237, 43)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "Apply Plan"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Panel10
+        '
+        Me.Panel10.Controls.Add(Me.Button1)
+        Me.Panel10.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel10.Location = New System.Drawing.Point(0, 120)
+        Me.Panel10.Name = "Panel10"
+        Me.Panel10.Size = New System.Drawing.Size(280, 64)
+        Me.Panel10.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.BackColor = System.Drawing.SystemColors.Control
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.Button1.Location = New System.Drawing.Point(27, 8)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(237, 43)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Register New Client"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'side_b1
+        '
+        Me.side_b1.Controls.Add(Me.home_btn)
+        Me.side_b1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.side_b1.Location = New System.Drawing.Point(0, 56)
+        Me.side_b1.Name = "side_b1"
+        Me.side_b1.Size = New System.Drawing.Size(280, 64)
+        Me.side_b1.TabIndex = 2
+        '
+        'home_btn
+        '
+        Me.home_btn.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.home_btn.BackColor = System.Drawing.SystemColors.Control
+        Me.home_btn.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.home_btn.FlatAppearance.BorderSize = 0
+        Me.home_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.home_btn.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.home_btn.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.home_btn.Location = New System.Drawing.Point(27, 8)
+        Me.home_btn.Name = "home_btn"
+        Me.home_btn.Size = New System.Drawing.Size(237, 43)
+        Me.home_btn.TabIndex = 0
+        Me.home_btn.Text = "Home"
+        Me.home_btn.UseVisualStyleBackColor = False
+        '
+        'Panel8
+        '
+        Me.Panel8.Controls.Add(Me.Label1)
+        Me.Panel8.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel8.Location = New System.Drawing.Point(0, 0)
+        Me.Panel8.Name = "Panel8"
+        Me.Panel8.Size = New System.Drawing.Size(280, 56)
+        Me.Panel8.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(23, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(241, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "St. Martha Memorial Homes Inc."
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.Panel9)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(22, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1216, 23)
+        Me.Panel1.TabIndex = 2
+        '
+        'Panel9
+        '
+        Me.Panel9.Controls.Add(Me.Label3)
+        Me.Panel9.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel9.Location = New System.Drawing.Point(1135, 0)
+        Me.Panel9.Name = "Panel9"
+        Me.Panel9.Size = New System.Drawing.Size(79, 21)
+        Me.Panel9.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label3.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.Label3.Location = New System.Drawing.Point(54, 2)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(18, 18)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "X"
+        '
+        'stats_panel
+        '
+        Me.stats_panel.BackColor = System.Drawing.SystemColors.Control
+        Me.stats_panel.Controls.Add(Me.Panel17)
+        Me.stats_panel.Controls.Add(Me.Panel16)
+        Me.stats_panel.Controls.Add(Me.Panel15)
+        Me.stats_panel.Dock = System.Windows.Forms.DockStyle.Top
+        Me.stats_panel.Location = New System.Drawing.Point(290, 67)
+        Me.stats_panel.Name = "stats_panel"
+        Me.stats_panel.Size = New System.Drawing.Size(904, 235)
+        Me.stats_panel.TabIndex = 3
+        '
+        'Panel16
+        '
+        Me.Panel16.Controls.Add(Me.panel_stat2)
+        Me.Panel16.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel16.Location = New System.Drawing.Point(339, 0)
+        Me.Panel16.Name = "Panel16"
+        Me.Panel16.Size = New System.Drawing.Size(565, 235)
+        Me.Panel16.TabIndex = 0
+        '
+        'Panel15
+        '
+        Me.Panel15.Controls.Add(Me.panel_stat1)
+        Me.Panel15.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel15.Location = New System.Drawing.Point(0, 0)
+        Me.Panel15.Name = "Panel15"
+        Me.Panel15.Size = New System.Drawing.Size(339, 235)
+        Me.Panel15.TabIndex = 0
+        '
+        'Panel17
+        '
+        Me.Panel17.Controls.Add(Me.panel_stat3)
+        Me.Panel17.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel17.Location = New System.Drawing.Point(615, 0)
+        Me.Panel17.Name = "Panel17"
+        Me.Panel17.Size = New System.Drawing.Size(289, 235)
+        Me.Panel17.TabIndex = 0
+        '
+        'panel_stat3
+        '
+        Me.panel_stat3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.panel_stat3.BackColor = System.Drawing.Color.Orange
+        Me.panel_stat3.Location = New System.Drawing.Point(65, 42)
+        Me.panel_stat3.Name = "panel_stat3"
+        Me.panel_stat3.Size = New System.Drawing.Size(179, 139)
+        Me.panel_stat3.TabIndex = 0
+        '
+        'panel_stat2
+        '
+        Me.panel_stat2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.panel_stat2.BackColor = System.Drawing.Color.Crimson
+        Me.panel_stat2.Location = New System.Drawing.Point(53, 42)
+        Me.panel_stat2.Name = "panel_stat2"
+        Me.panel_stat2.Size = New System.Drawing.Size(179, 139)
+        Me.panel_stat2.TabIndex = 0
+        '
+        'panel_stat1
+        '
+        Me.panel_stat1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.panel_stat1.BackColor = System.Drawing.Color.ForestGreen
+        Me.panel_stat1.Location = New System.Drawing.Point(74, 42)
+        Me.panel_stat1.Name = "panel_stat1"
+        Me.panel_stat1.Size = New System.Drawing.Size(179, 139)
+        Me.panel_stat1.TabIndex = 0
+        '
+        'Dashboard
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1238, 583)
+        Me.Controls.Add(Me.dash_holder)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.p_left)
+        Me.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Dashboard"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Dashboard"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.dash_holder.ResumeLayout(False)
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel5.PerformLayout()
+        Me.Panel7.ResumeLayout(False)
+        Me.Panel12.ResumeLayout(False)
+        Me.Panel11.ResumeLayout(False)
+        Me.Panel10.ResumeLayout(False)
+        Me.side_b1.ResumeLayout(False)
+        Me.Panel8.ResumeLayout(False)
+        Me.Panel8.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel9.ResumeLayout(False)
+        Me.Panel9.PerformLayout()
+        Me.stats_panel.ResumeLayout(False)
+        Me.Panel16.ResumeLayout(False)
+        Me.Panel15.ResumeLayout(False)
+        Me.Panel17.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents p_left As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents dash_holder As Panel
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel7 As Panel
+    Friend WithEvents Panel8 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel9 As Panel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel12 As Panel
+    Friend WithEvents Panel13 As Panel
+    Friend WithEvents Panel11 As Panel
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Panel10 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents side_b1 As Panel
+    Friend WithEvents home_btn As Button
+    Friend WithEvents stats_panel As Panel
+    Friend WithEvents Panel17 As Panel
+    Friend WithEvents panel_stat3 As Panel
+    Friend WithEvents Panel16 As Panel
+    Friend WithEvents panel_stat2 As Panel
+    Friend WithEvents Panel15 As Panel
+    Friend WithEvents panel_stat1 As Panel
+End Class
---
+++ StMarthaDashBoard/Login.vb
@@ -0,0 +1,5 @@
+﻿Public Class Login
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+        Application.Exit()
+    End Sub
+End Class
